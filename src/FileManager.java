0a8bc4eba1dca840c960acad2e7ecedd0f0690f7
LuisMiguelB <luibenmor@gmail.com>
2024-04-29 22:59:57 +0200

    Basic Functionality

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,2 +1,113 @@
-package PACKAGE_NAME;public class FileManager {
+import java.io.File;
+import java.util.Arrays;
+
+public class FileManager {
+
+    public FileManager() {
+    }
+
+    File path = new File(System.getProperty("user.dir"));
+
+    public void pwd() {
+        System.out.println(path);
+    }
+
+    public void cd(String dir) {
+        if (dir.equals("..")) {
+            if (path.getParentFile() != null) {
+                path = path.getParentFile();
+            } else {
+                System.out.println("Already at the root directory");
+            }
+        } else {
+            File newPath = new File(path, dir);
+            if (newPath.exists() && newPath.isDirectory()) {
+                path = newPath;
+            } else {
+                System.out.println("Directory does not exist or is not a directory");
+            }
+        }
+    }
+
+    public void ls() {
+        File[] files = path.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                System.out.println("[D] " + file.getName());
+            }
+        }
+        for (File file : files) {
+            if (file.isFile()) {
+                System.out.println("[A] " + file.getName());
+            }
+        }
+    }
+
+    public void ll() {
+        File[] files = path.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                System.out.println("[D] " + file.getName() + " | " + file.length() + " bytes | Modified: " + file.lastModified());
+            }
+        }
+        for (File file : files) {
+            if (file.isFile()) {
+                System.out.println("[A] " + file.getName() + " | " + file.length() + " bytes | Modified: " + file.lastModified());
+            }
+        }
+    }
+
+    public void mkdir(String dirName) {
+        File newDir = new File(path, dirName);
+        boolean isCrated = newDir.mkdir();
+        if (isCrated) {
+            System.out.println("Directory created successfully");
+        } else {
+            System.out.println("Failed to create directory");
+        }
+    }
+
+    public void rm(String fileName) {
+        File fileToDelete = new File(path, fileName);
+        if (!fileToDelete.exists()) {
+            System.out.println("File or directory does not exist");
+        } else {
+            if (fileToDelete.isDirectory() && fileToDelete.list().length > 0) {
+                System.out.println("Directory is not empty");
+            } else {
+                boolean isDeleted = fileToDelete.delete();
+                if (isDeleted) {
+                    System.out.println("File or directory deleted successfully");
+                } else {
+                    System.out.println("Failed to delete file or directory");
+                }
+            }
+        }
+    }
+
+    public void mv(String oldFileName, String newFileName) {
+        File oldFile = new File(path, oldFileName);
+        if (!oldFile.exists()) {
+            System.out.println("File or directory does not exist");
+        } else {
+            File newFile = new File(path, newFileName);
+            boolean isMoved = oldFile.renameTo(newFile);
+            if (isMoved) {
+                System.out.println("File or directory moved or renamed successfully");
+            } else {
+                System.out.println("Failed to move or rename file or directory");
+            }
+        }
+    }
+
+    public void help() {
+        System.out.println("- - pwd - -\nMuestra la carpeta actual.");
+        System.out.println("- - cd <DIR> - -\nCambia la carpeta actual a ‘DIR’. Con .. cambia a la carpeta superior.");
+        System.out.println("- - ls - -\nMuestra la lista de directorios y archivos de la carpeta actual (primero directorios, luego archivos, ambos ordenados alfabéticamente).");
+        System.out.println("- - ll - -\nComo ls pero muestra también el tamaño y la fecha de última modificación.");
+        System.out.println("- - mkdir <DIR> - -\nCrea el directorio ‘DIR’ en la carpeta actual.");
+        System.out.println("- - rm <FILE> - -\nBorra ‘FILE’. Si es una carpeta, borrará primero sus archivos y luego la carpeta. Si tiene subcarpetas, las dejará intactas y mostrará un aviso al usuario.");
+        System.out.println("- - mv <FILE1> <FILE2> - -\nMueve o renombra ‘FILE1’ a ‘FILE2’ ");
+    }
+
 }
--- src/Terminal.java
+++ src/Terminal.java
@@ -1,2 +1,78 @@
-package PACKAGE_NAME;public class MiniTerminal {
+import java.io.File;
+import java.util.Scanner;
+
+public class Terminal {
+
+    public static void main(String[] args) {
+
+        FileManager fileManager = new FileManager();
+
+        Scanner sc = new Scanner(System.in);
+        boolean exit = false;
+
+        do {
+
+            System.out.print("luismi@laptop ~ $ ");
+
+            String input = sc.nextLine();
+            String command, argument = null;
+
+            if (input.contains(" ")) {
+                String[] parts = input.split(" ");
+                command = parts[0];
+                argument = parts[1];
+            } else {
+                command = input;
+            }
+
+            switch (command) {
+
+                case "pwd":
+                    fileManager.pwd();
+                    break;
+
+                case "cd":
+                    fileManager.cd(argument);
+                    break;
+
+                case "ls":
+                    fileManager.ls();
+                    break;
+
+                case "ll":
+                    fileManager.ll();
+                    break;
+
+                case "mkdir":
+                    fileManager.mkdir(argument);
+                    break;
+
+                case "rm":
+                    fileManager.rm(argument);
+                    break;
+
+                case "mv":
+                    System.out.println("Function not available");
+                    // Function created in FileManager but not implemented
+                    // if argument2/parts[2] is added, all explodes
+                    break;
+
+                case "help":
+                    fileManager.help();
+                    break;
+
+                case "exit":
+                    exit = true;
+                    break;
+
+                default:
+                    System.out.println("Order failed");
+                    break;
+
+            }
+
+        } while (!exit);
+
+    }
+
 }
